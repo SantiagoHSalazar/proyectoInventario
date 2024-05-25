0e1a1621fa90a9c6556f58d7a0689be36c0fefae
SantiagoHSalazar <santiagohsalazar@gmail.com>
2024-05-25 17:17:49 -0500

    proyecto Inventario Seminario

---
+++ Controladores/ControladorCategoria.py
@@ -0,0 +1,32 @@
+from Repositorio.RepositorioCategoria import RepositorioCategoria
+from Modelos.Categoria import Categoria
+
+class ControladorCategoria():
+
+    def __init__(self):
+        self.repositorioCategoria = RepositorioCategoria()
+
+    def __index__(self):
+        return self.repositorioCategoria.findAll()
+
+    def create(self,infoCategoria):
+        nuevaCategoria = Categoria(infoCategoria)
+        return self.repositorioCategoria.save()
+
+    def show(self,id):
+        laCategoria = Categoria(self.repositorioCategoria.findById(id))
+        return laCategoria.__dict__
+
+    def update(self,id, infoCategoria):
+        categoriaActual = Categoria(self.repositorioCategoria.findById(id))
+        categoriaActual.nombre = infoCategoria["nombre"]
+        categoriaActual.descripcion = infoCategoria["descripcion"]
+        categoriaActual.fechaCreacion = infoCategoria["fechaCreacion"]
+        return self.repositorioCategoria.save(categoriaActual)
+
+    def delete(self,id):
+        return self.repositorioCategoria.delete(id)
+
+
+
+
