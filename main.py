4951ee5800c08fa1aef9c46fc436d1589dde5524
SantiagoHSalazar <santiagohsalazar@gmail.com>
2024-06-13 15:15:24 -0500

    proyecto Inventario Seminario

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from flask_cors import CORS
 import json
 from waitress import serve
 from Controladores.ControladorEmpleado import ControladorEmpleado
+from Controladores.ControladorCategoria import ControladorCategoria
+from Controladores.ControladorProducto import ControladorProducto
+from Controladores.ControladorInventario import ControladorInventario
 import pymongo
 import certifi
 
@@ -17,6 +20,10 @@ db = client.test
 print(db)
 
 miControladorEmpleado = ControladorEmpleado()
+miControladorCategoria = ControladorCategoria()
+miControladorProducto = ControladorProducto()
+miControladorInventario = ControladorInventario()
+
 
 @app.route("/emplado",methods=['GET'])
 def getEmplado():
@@ -43,6 +50,98 @@ def modificarEmpleado(id):
 @app.route("/empleado/<string:id>",methods=['DELETE'])
 def eliminarEmpleado(id):
     json=miControladorEmpleado.delete(id)
+    return jsonify(json)
+
+####################################################################################
+
+@app.route("/categoria/<string:id>",methods=['GET'])
+def getCategorias():
+    json = miControladorCategoria.index()
+    return jsonify(json)
+
+@app.route("/categoria/<string:id>",methods=['GET'])
+def getCategoria(id):
+    json=miControladorCategoria.show(id)
+    return jsonify(json)
+
+@app.route("/categoria/<string:id>",methods=['POST'])
+def crearCategoria():
+    data = request.get_json()
+    json = miControladorCategoria.create(data)
+    return jsonify(json)
+
+@app.route("/categoria/<string:id>",methods=['PUT'])
+def modificarCategoria(id):
+    data = request.get_json()
+    json = miControladorCategoria.update(id, data)
+    return jsonify(json)
+
+@app.route("/categoria/<string:id>", methods=['DELETE'])
+def eliminarCategoria(id):
+    json = miControladorCategoria.delete(id)
+    return jsonify(json)
+#######################################################################################
+
+@app.route("/producto", methods=['GET'])
+def getProductos():
+    json = miControladorProducto.index()
+    return jsonify(json)
+
+@app.route("/producto/<string:id>", methods=['GET'])
+def getProducto(id):
+    json = miControladorProducto.show(id)
+    return jsonify(json)
+
+@app.route("/producto", methods=['POST'])
+def crearProducto():
+    data = request.get_json()
+    json = miControladorProducto.create(data)
+    return jsonify(json)
+
+@app.route("/producto/<string:id>", methods=['PUT'])
+def modificarProducto(id):
+    data = request.get_json()
+    json = miControladorProducto.update(id, data)
+    return jsonify(json)
+
+@app.route("/producto/<string:id>", methods=['DELETE'])
+def eliminarProducto(id):
+    json = miControladorProducto.delete(id)
+    return jsonify(json)
+
+@app.route("/producto/<string:id>/categoria/<string:id_categoria>", methods=['PUT'])
+def asignarCategoriaAProducto(id, id_categoria):
+    json = miControladorProducto.asignarCategoria(id, id_categoria)
+    return jsonify(json)
+#######################################################################################################
+
+@app.route("/inventario", methods=['GET'])
+def getInventarios():
+    json = miControladorInventario.index()
+    return jsonify(json)
+
+
+@app.route("/inventario/<string:id>", methods=['GET'])
+def getInventario(id):
+    json = miControladorInventario.show(id)
+    return jsonify(json)
+
+@app.route("/inventario/empleado/<string:identificacion>/producto/<string:id_producto>", methods=['POST'])
+def crearInventario(identificacion, id_producto):
+    data = request.get_json()
+    json = miControladorInventario.create(data, identificacion, id_producto)
+    return jsonify(json)
+
+
+@app.route("/inventario/<string:id_inventario>/empleado/<string:identificacion>/producto/<string:id_producto>", methods=['PUT'])
+def modificarInventario(id_inventario, identificacion, id_producto):
+    data = request.get_json()
+    json = miControladorInventario.update(id_inventario, data, identificacion, id_producto)
+    return jsonify(json)
+
+@app.route("/inventario/<string:id_inventario>", methods=['DELETE'])
+def eliminarInventario(id_inventario):
+    json = miControladorInventario.delete(id_inventario)
     return jsonify(json)
 
 @app.route("/", methods=['GET'])
